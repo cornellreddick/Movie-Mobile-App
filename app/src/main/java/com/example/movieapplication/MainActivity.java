9988db93c9628d2144c9079d62d89fbaae53e86c
Cornell Reddick <cornellreddick@outlook.com>
2022-03-19 13:06:23 -0400

    initial commit.

---
+++ app/src/main/java/com/example/movieapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.movieapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.movieapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
